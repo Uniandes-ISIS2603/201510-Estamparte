7684fb95a2e16e2272010fbdf921c3bed9561fe3
SamuelSSalazar <samuel.s.salazar@gmail.com>
2015-03-05 11:01:46 -0500

    Se quita mas codigo viejo

--- 201510-Estamparte-master/Documents/NetBeansProjects/Estampate/public_html/src/Modules/formaPago/formaPago.ctrl.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	var app = angular.module('formaPagoModule');
-
-	app.controller('formaPagoCtrl', ['$scope', 'CRUDUtils', 'formaPago.context', function ($scope, CRUDUtils, context) {
-			this.url = context;
-			CRUDUtils.extendCtrl(this, $scope);
-			this.fetchRecords();
-		}]);
-})();
