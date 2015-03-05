20a6a197e31f9a8a9f44db11566342020c2e89e6
SamuelSSalazar <samuel.s.salazar@gmail.com>
2015-03-05 11:07:44 -0500

    borrado de todo

--- public_html/src/modules/barra/barra.ctrl.js
+++
@@ -1,7 +0,0 @@
-angular.module('barraModule', [])
-.controller('barraCtrl', ['$scope', function($scope) {}])
-.directive('barra', function() {
-  return {
-    templateUrl: 'src/modules/barra/barra.tpl.html'
-  };
-});
