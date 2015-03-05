3c3a37ace403b3c940c8215e8865f6623d2a1621
SamuelSSalazar <samuel.s.salazar@gmail.com>
2015-03-05 12:23:18 -0500

    Actualizacion del controlador y el template de camiseta.
    Se actualizó el controlador y el template de camiseta para que
    funcionara de acuerdo a las variables globales declaradas por
    convención.

--- src/modules/camiseta/camiseta.ctrl.js
+++ src/modules/camiseta/camiseta.ctrl.js
@@ -9,34 +9,44 @@
     app.controller('camisetaCtrl', ['$scope', 'CRUDUtils', 'camiseta.context', function ($scope, CRUDUtils, context) {
             this.url = context;
             CRUDUtils.extendCtrl(this, $scope);
-            
+            $scope.datoActual.estampasUsadas = [];
 
             this.contiene = function (id)
             {
-                for (var i = 0; i < $scope.datos.length; i++)
+                for (var i = 0; i < $scope.datoActual.estampasUsadas.length; i++)
                 {
-                    if ($scope.datos[i] === id)
+                    if ($scope.datoActual.estampasUsadas[i] === id)
                         return true;
                 }
                 return false;
             };
             this.agregarEstampa = function (id)
             {
-                $scope.datos.push(id);
+                $scope.datoActual.estampasUsadas.push(id);
             };
             this.eliminarEstampa = function (id)
             {
 
-                for (var i = 0; i < $scope.datos.length; i++)
+                for (var i = 0; i < $scope.datoActual.estampasUsadas.length; i++)
                 {
-                    if ($scope.datos[i] === id)
+                    if ($scope.datoActual.estampasUsadas[i] === id)
                     {
-                        $scope.datos.splice(i, 1);
+                        $scope.datoActual.estampasUsadas.splice(i, 1);
                         break;
                     }
                 }
             };
 
+
+
+            this.continuar = function () {
+                var msj = "";
+                for (var i = 0; i < $scope.datoActual.estampasUsadas.length; i++)
+                    if ($scope.datoActual.estampasUsadas[i])
+                        msj += (i + 1) + ")" + $scope.datoActual.estampasUsadas[i].valueOf() + "\n\t";
+                alert("\tDETALLES:\n\tEstilo:\t" + $scope.datoActual.selected.estilo + "\n\tTalla:\t" + $scope.datoActual.selected.talla + "\n\tColor:\t" + $scope.datoActual.selected.color
+                        + "\n\tESTAMPAS USADAS:\n\t" + msj);
+            };
         }]);
 })();
 
