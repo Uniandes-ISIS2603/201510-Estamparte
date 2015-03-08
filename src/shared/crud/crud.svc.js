a94322f6bf8668aea7e81f7e40199ac359313519
Juan Sebastian <jsmarti@hotmail.com>
2015-03-08 15:44:09 -0500

    Actualizacion de servicios CRUD con paginación

--- src/shared/crud/crud.svc.js
+++ src/shared/crud/crud.svc.js
@@ -1,58 +1,72 @@
-(function(){
-    
+(function () {
     var crud = angular.module('CrudModule');
-    
-    crud.factory('CRUDUtils',['Restangular',function(Restangular){
-            
-            function CRUD($scope){
-                
-                this.api = Restangular.all(this.url);
-                $scope.datoActual={};
-                $scope.datos=[];
-                
-            this.consultarDatos = function () {
-                    this.api.getList().then(function (data) {
-                            $scope.datos = data;
-                            $scope.datoActual = {};
-                    });
-                    
-            };
-            this.crearDato = function () {
-                    
-                    $scope.datoActual = {};
-                   
-            };
-            this.guardarDato = function () {
-                    
-                    if ($scope.datoActual.id) {
-                            $scope.datoActual.put().then(function () {
-                                    $scope.datoActual = {};
-                            });
+
+    crud.factory('CRUDUtils', ['Restangular', function (RestAngular) {
+            function crudConstructor() {
+                this.api = RestAngular.all(this.url);
+
+                this.consultarDatos = function (paginaActual, itemsPorPagina) {
+                    return this.api.getList(null, {page: paginaActual, maxRecords: itemsPorPagina});
+                };
+                this.guardarDato = function (datoActual) {
+                    if (datoActual.id) {
+                        return datoActual.put();
                     } else {
-                            this.api.post($scope.datoActual).then(function () {
-                                    $scope.datoActual = {};
-                            });
+                        return this.api.post(datoActual);
                     }
-                  
-            };
-            this.eliminarDato = function (dato) {
-                   
-                    dato.remove().then(function () {
-                            $scope.datoActual = {};
-                    });
-                    
-            };
-            this.editarDato = function (dato) {
-                    $scope.datoActual = Restangular.copy(dato);
-                    
-            };
-                
-            };
-            return {
-                extendCtrl: function(obj,scope){
-                    CRUD.call(obj,scope);
-                }
-            };
-    }]);
-    
+                };
+                this.eliminarDato = function (dato) {
+                    return dato.remove();
+                };
+                this.extendCtrl = function (ctrl, scope) {
+                    //Variables para el scope
+                    scope.datoActual = {};
+                    scope.datos = [];
+
+                    //Variables de paginacion
+                    scope.maxSize = 5;
+                    scope.itemsPorPagina = 5;
+                    scope.totalItems = 0;
+                    scope.paginaActual = 1;
+
+
+                    //Funciones que no requieren del servicio
+                    ctrl.crearDato = function () {
+                        scope.datoActual = {};
+                    };
+                    ctrl.editarDato = function (dato) {
+                        scope.datoActual = RestAngular.copy(dato);
+                    };
+
+                    //Funciones que usan el servicio CRUD
+                    var service = this;
+
+                    ctrl.pageChanged = function () {
+                        this.consultarDatos();
+                    };
+
+                    ctrl.consultarDatos = function () {
+                        return service.consultarDatos(scope.paginaActual, scope.itemsPorPagina).then(function (data) {
+                            scope.datos = data;
+                            scope.totalItems = data.totalRecords;
+                            scope.datoActual = {};
+                            return data;
+                        });
+                    };
+                    ctrl.guardarDato = function () {
+                        return service.guardarDato(scope.datoActual).then(function () {
+                            ctrl.consultarDatos();
+                        });
+                    };
+                    ctrl.eliminarDato = function (dato) {
+                        return service.eliminarDato(dato).then(function () {
+                            ctrl.consultarDatos();
+                        });
+                    };
+                };
+            }
+            return {extendService: function (svc) {
+                    crudConstructor.call(svc);
+                }};
+        }]);
 })();
