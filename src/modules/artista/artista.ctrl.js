42596cfd085352b7207967ce68d5e5f2a4769779
Juan Sebastian <jsmarti@hotmail.com>
2015-03-09 17:25:19 -0500

    Modificacion de template de artista

--- src/modules/artista/artista.ctrl.js
+++ src/modules/artista/artista.ctrl.js
@@ -10,6 +10,7 @@
 	app.controller('artistaCtrl', ['$scope', 'servicioArtista','servicioEstampa', function ($scope, servicioArtista,servicioEstampa) {
 			servicioArtista.extendCtrl(this,$scope);
                         this.crearEstampaTpl = false;
+                        this.editarEstampa=false;
                         
                         //Temas para la creacion de una estampa
                         temas=['TemaA','TemaB','TemaC'];
@@ -28,14 +29,22 @@
                             estampa.id = this.idEstampa;
                             //El usuario debe ser configurado en $scope!!!!
                             estampa.idAutor = $scope.idUsuario;
+                            estampa.usuarioAutor = $scope.usuario;
                             this.idEstampa = this.idEstampa + 1;
                             servicioEstampa.guardarDato(estampa);
                             $scope.datoActual=estampa;
+                            this.popEditarEstampa();
                         };
                         
                         this.cancelarCrearEstampa = function(){
                             this.crearEstampaTpl=false;
                         };
+                        
+                        this.popEditarEstampa = function(){
+                            this.editarEstampa=true;
+                        };
+                        
+
                         
 		}]);
     
