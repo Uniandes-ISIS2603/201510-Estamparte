a28e8cbc7b65e6996a48e2670f182fb8b9f9d2a4
Meili Vanegas <m.vanegas10@uniandes.edu.co>
2015-03-12 10:53:29 -0500

    Funciones para conectar carrito con camiseta

--- src/modules/carrito/carrito.ctrl.js
+++ src/modules/carrito/carrito.ctrl.js
@@ -3,7 +3,43 @@
    var app = angular.module('carritoModule');
 
 	app.controller('carritoCtrl', ['$scope', 'servicioCarrito', function ($scope, serviciocarrito) {
-			serviciocarrito.extendCtrl(this,$scope);    
+			serviciocarrito.extendCtrl(this,$scope);
+                       
+                        this.popCrearCamiseta= function(){
+                            this.crearDato();
+                            this.crearCamisetaTpl=true;
+                            this.editarCamiseta=false;
+                        };
+                        
+                        
+                        this.crearCamiseta = function(camiseta){
+                            //El usuario debe ser configurado en $rootScope!!!!
+                            $rootScope.datoActual=camiseta;
+                            alert("Camiseta: " + camiseta.id + camiseta.nombre + camiseta.talla + camiseta.estilo + camiseta.color );
+                        };
+                        
+                        this.cancelarCrearCamiseta = function(){
+                            this.crearCamisetaTpl=false;
+                        };
+                        
+                        this.popEditarCamiseta = function(){
+                            this.crearCamisetaTpl=false;
+                            this.editarCamiseta=true;
+                        };
+                        
+                        this.buscarCamisetasPropias=function(){
+                            servicioCamiseta.camisetasDeComprador().then(function(data){
+                                $scope.datos=[];
+                                var r=''
+                                for(var i=0;i<data.length;i++){
+                                   r=r+'Nombre de camiseta: '+data[i].nombre+'\n ';
+                                }
+                                alert(r);
+                           });
+                        };
+                        
+                        
+
 		}]);
     
 })();
