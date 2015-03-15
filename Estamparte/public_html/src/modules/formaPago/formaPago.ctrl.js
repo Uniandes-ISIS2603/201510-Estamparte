88536bb5e34eb18e0932a75e39e3e1ee8fc7be14
jsmarti <jsmarti@hotmail.com>
2015-03-15 17:29:22 -0500

    error

--- Estamparte/public_html/src/modules/formaPago/formaPago.ctrl.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    
-   var app = angular.module('formaPagoModule');
-
-	app.controller('formaPagoCtrl', ['$scope', 'servicioFormaPago', function ($scope, servicioFormaPago) {
-			servicioFormaPago.extendCtrl(this,$scope);    
-		}]);
-    
-})();
--- Estamparte/public_html/src/modules/inicio/inicio.ctrl.js
+++
@@ -1,7 +0,0 @@
-angular.module('inicioModule', [])
-.controller('inicioCtrl', ['$scope', function($scope) {}])
-.directive('inicio', function() {
-	return {
-		templateUrl: 'src/modules/inicio/inicio.tpl.html'
-	};
-});
