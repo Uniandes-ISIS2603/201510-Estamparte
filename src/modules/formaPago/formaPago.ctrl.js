60fcc1d1c3013852d3c6a518e71168446c56cfe7
Meili Vanegas <m.vanegas10@uniandes.edu.co>
2015-03-16 22:16:34 -0500

    Métodos en forma de pago service para conexión con otro módulos

--- src/modules/formaPago/formaPago.ctrl.js
+++ src/modules/formaPago/formaPago.ctrl.js
@@ -2,8 +2,23 @@
     
    var app = angular.module('formaPagoModule');
 
-	app.controller('formaPagoCtrl', ['$scope', 'servicioFormaPago', function ($scope, servicioFormaPago) {
-			servicioFormaPago.extendCtrl(this,$scope);    
-		}]);
+	app.controller('formaPagoCtrl', ['$scope', 'servicioFormaPago', '$location','$rootScope', function ($scope, servicioFormaPago, $location, $rootScope) {
+            servicioFormaPago.extendCtrl(this,$scope);    
+            
+            datos = [
+            {  
+                tipo: "Visa",
+                numeroTarjeta: 123456789
+                fechaVencimiento: ("October 13, 2014 11:13:00"),
+                siGusta: 381,
+                noGusta: 212,
+                precio: 100,
+                imagenes: [
+                    'src/assets/img/estampa.jpg'
+                ],
+                tema: 'TemaA'
+            }
+            
+        }]);
     
 })();
--- src/modules/formaPago/formaPago.svc.js
+++ src/modules/formaPago/formaPago.svc.js
@@ -1,12 +1,33 @@
 (function(){
     var formaPagoModule = angular.module('formaPagoModule');
-    formaPagoModule.controller('formaPagoModule', ['$scope', 'servicioFormaPago', '$rootscope', function ($scope, servicioFormaPago, $rootscope) {
+    formaPagoModule.service('ServicioFormaPago', ['CRUDUtils','formaPago.context',function(CRUDUtils,context){
         this.url = context;
 	CRUDUtils.extendService(this);
-    }]);
-    
-    this.darFormaPagoActual = function(){
-        return $scope.datoActual;
-    }
+        
+        //FormaPago seleccionada en particular
+        this.formaPagoSeleccionada={};
 
+        //FormaPagos disponibles para un cliente
+        this.formaPagosDisponibles=[];
+
+        //Actualiza una formaPago seleccionada
+        this.setFormaPagoCreada = function(formaPago){
+            this.formaPagoSeleccionada=formaPago;
+        };
+
+        //Agrega una formaPago a las formaPagos disponibles para el cliente
+        this.addFormaPagoDisponible=function(formaPago){
+            this.formaPagosDisponibles.push(formaPago);
+        };
+
+        //Obtiene las formaPagos registradas para un cliente
+        this.formaPagosCliente=function(){
+            return this.api.customGET('formaPagosCliente');
+        };
+
+        this.darFormaPagoActual = function(){
+            return $scope.datoActual;
+        }
+
+    }]);
 })();
