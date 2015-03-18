155a78d0a771cd1394ce6a614f1ce4109c35a2f8
jsmarti <jsmarti@hotmail.com>
2015-03-18 09:00:52 -0500

    Generacion de paquetes y clases para el backend de estampas

---
+++ Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/estampa/logic/converter/EstampaConverter.java
@@ -0,0 +1,8 @@
+/*
+ * Conversor de datos de estampas
+ */
+package co.edu.uniandes.estamparte.estampa.logic.converter;
+
+public class EstampaConverter {
+    
+}
---
+++ Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/estampa/logic/entity/EstampaEntity.java
@@ -0,0 +1,8 @@
+/*
+ * Entidad de estampa
+ */
+package co.edu.uniandes.estamparte.estampa.logic.entity;
+
+public class EstampaEntity {
+    
+}
