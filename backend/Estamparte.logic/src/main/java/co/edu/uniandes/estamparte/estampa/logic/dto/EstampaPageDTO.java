f1cc0766df292d3c6759b572f40d17d62bb20d9b
jsmarti <jsmarti@hotmail.com>
2015-03-19 16:56:07 -0500

    DTO de pagina de estampas completo

--- backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/estampa/logic/dto/EstampaPageDTO.java
+++ backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/estampa/logic/dto/EstampaPageDTO.java
@@ -3,6 +3,42 @@
  */
 package co.edu.uniandes.estamparte.estampa.logic.dto;
 
+import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class EstampaPageDTO {
+    
+    //Atributos
+    
+    //Numero total de estampas en la pagina
+    private Long totalEstampas;
+    
+    //Lista de estampas en la pagina
+    private List<EstampaDTO> estampas;
+    
+    //Metodos analizadores
+    
+    //Retorna el numero de estampas
+    public Long darTotalEstampas(){
+        return totalEstampas;
+    }
+    
+    //Retorna la lista de estampas
+    public List<EstampaDTO> darEstampas(){
+        return estampas;
+    }
+    
+    //Metodos modificadores
+    
+    //Asigna el numero total de estampas por la pagina
+    public void cambiarTotalEstampas(Long nTotalEstampas){
+        this.totalEstampas = nTotalEstampas;
+    }
+    
+    //Asigna la lista de estampas en la pagina
+    public void cambiarEstampas(List<EstampaDTO> nEstampas){
+        this.estampas = nEstampas;
+    } 
     
 }
