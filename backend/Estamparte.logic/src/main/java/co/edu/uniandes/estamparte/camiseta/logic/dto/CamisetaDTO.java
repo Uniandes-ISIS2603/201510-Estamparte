19f597ba12b0cce9600a9e66507242fbde853dcb
SamuelSSalazar <samuel.s.salazar@gmail.com>
2015-03-19 09:47:40 -0500

    Backend camiseta (faltando logic)

--- backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/camiseta/logic/dto/CamisetaDTO.java
+++ backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/camiseta/logic/dto/CamisetaDTO.java
@@ -4,8 +4,82 @@
 package co.edu.uniandes.estamparte.camiseta.logic.dto;
 
 import co.edu.uniandes.estamparte.estampa.logic.dto.*;
+import java.util.ArrayList;
 
 
 public class CamisetaDTO {
+    
+    public String id;
+    
+    public ArrayList<EstampaDTO> estampas;
+    
+    public String talla;
+    
+    public String color;
+    
+    public String estilo;
+    
+    public String nombre;
+    
+    public double costo;
+    
+    public String darId() {
+        return id;
+    }
+
+    public void asignarId(String id) {
+        this.id = id;
+    }
+
+    public ArrayList<EstampaDTO> darEstampas() {
+        return estampas;
+    }
+
+    public void asignarEstampas(ArrayList<EstampaDTO> estampas) {
+        this.estampas = estampas;
+    }
+
+    public String darTalla() {
+        return talla;
+    }
+
+    public void asignarTalla(String talla) {
+        this.talla = talla;
+    }
+
+    public String darColor() {
+        return color;
+    }
+
+    public void asignarColor(String color) {
+        this.color = color;
+    }
+
+    public String darEstilo() {
+        return estilo;
+    }
+
+    public void asignarEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public String darNombre() {
+        return nombre;
+    }
+
+    public void asignarNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double darCosto() {
+        return costo;
+    }
+
+    public void asignarCosto(double costo) {
+        this.costo = costo;
+    }
+    
+    
+    
     
 }
