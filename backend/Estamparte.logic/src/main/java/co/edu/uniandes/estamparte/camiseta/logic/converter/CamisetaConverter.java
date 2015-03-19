a82d2f9581ca616b94a4683a5770dae7881b2b4b
Meili Vanegas <m.vanegas10@uniandes.edu.co>
2015-03-18 21:05:47 -0500

    Converter: Artista, Camiseta, Estampa, Carrito, FormaPago, Comprador

--- backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/camiseta/logic/converter/CamisetaConverter.java
+++ backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/camiseta/logic/converter/CamisetaConverter.java
@@ -3,8 +3,48 @@
  */
 package co.edu.uniandes.estamparte.camiseta.logic.converter;
 
-import co.edu.uniandes.estamparte.estampa.logic.converter.*;
+//Convierte los objetos de clases de java a DTO (objeto que transfiere archivos a persistencia)
+
+import co.edu.uniandes.estamparte.camiseta.logic.dto.CamisetaDTO;
+import co.edu.uniandes.estamparte.camiseta.logic.entity.CamisetaEntity;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CamisetaConverter {
-    
+    public static CamisetaDTO convertirDeEntidadADTO (CamisetaEntity entity) {
+        if (entity != null) {
+            CamisetaDTO dto = new CamisetaDTO();
+            //Al objeto DTO se le asignan los atributos del objeto entity
+            return dto;
+        } else {
+            return null;
+        }
+    }
+
+    public static CamisetaEntity convertirDeDTOAEntidad (CamisetaDTO dto) {
+        if (dto != null) {
+            CamisetaEntity entity = new CamisetaEntity();
+            //Al objeto entity se le asignan los atributos del objeto dto
+            return entity;
+        } else {
+            return null;
+        }
+    }
+
+    public static List<CamisetaDTO> convertirDeListaEntidadesAListaDTO (List<CamisetaEntity> listaEntities) {
+        List<CamisetaDTO> listaDTO = new ArrayList<CamisetaDTO>();
+        for (CamisetaEntity entity : listaEntities) {
+            listaDTO.add(convertirDeEntidadADTO(entity));
+        }
+        return listaDTO;
+    }
+
+    public static List<CamisetaEntity> convertirDeListaDTOAListaEntidades (ArrayList<CamisetaDTO> listaDTO) {
+        List<CamisetaEntity> listaEntities = new ArrayList<CamisetaEntity>();
+        for (CamisetaDTO dto : listaDTO) {
+            listaEntities.add(convertirDeDTOAEntidad(dto));
+        }
+        return listaEntities;
+    }
+     
 }
--- backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/comprador/logic/converter/CompradorConverter.java
+++ backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/comprador/logic/converter/CompradorConverter.java
@@ -3,11 +3,49 @@
  */
 package co.edu.uniandes.estamparte.comprador.logic.converter;
 
-import co.edu.uniandes.estamparte.carrito.logic.converter.*;
-import co.edu.uniandes.estamparte.artista.logic.converter.*;
-import co.edu.uniandes.estamparte.camiseta.logic.converter.*;
-import co.edu.uniandes.estamparte.estampa.logic.converter.*;
+//Convierte los objetos de clases de java a DTO (objeto que transfiere archivos a persistencia)
+
+import co.edu.uniandes.estamparte.comprador.logic.dto.CompradorDTO;
+import co.edu.uniandes.estamparte.comprador.logic.entity.CompradorEntity;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CompradorConverter {
-    
+    public static CompradorDTO convertirDeEntidadADTO (CompradorEntity entity) {
+        if (entity != null) {
+            CompradorDTO dto = new CompradorDTO();
+            //Al objeto DTO se le asignan los atributos del objeto entity
+            return dto;
+        } else {
+            return null;
+        }
+    }
+
+    public static CompradorEntity convertirDeDTOAEntidad (CompradorDTO dto) {
+        if (dto != null) {
+            CompradorEntity entity = new CompradorEntity();
+            //Al objeto entity se le asignan los atributos del objeto dto
+            return entity;
+        } else {
+            return null;
+        }
+    }
+
+    public static List<CompradorDTO> convertirDeListaEntidadesAListaDTO (List<CompradorEntity> listaEntities) {
+        List<CompradorDTO> listaDTO = new ArrayList<CompradorDTO>();
+        for (CompradorEntity entity : listaEntities) {
+            listaDTO.add(convertirDeEntidadADTO(entity));
+        }
+        return listaDTO;
+    }
+
+    public static List<CompradorEntity> convertirDeListaDTOAListaEntidades (ArrayList<CompradorDTO> listaDTO) {
+        List<CompradorEntity> listaEntities = new ArrayList<CompradorEntity>();
+        for (CompradorDTO dto : listaDTO) {
+            listaEntities.add(convertirDeDTOAEntidad(dto));
+        }
+        return listaEntities;
+    }
+     
 }
+
