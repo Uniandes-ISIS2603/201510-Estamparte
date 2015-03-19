1c010a3edb105e678dd43fd8765dedad1dfdf9df
jsmarti <jsmarti@hotmail.com>
2015-03-19 12:31:53 -0500

    API de estampa completo

--- backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/estampa/logic/api/IEstampaLogic.java
+++ backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/estampa/logic/api/IEstampaLogic.java
@@ -3,7 +3,29 @@
  */
 package co.edu.uniandes.estamparte.estampa.logic.api;
 
+import co.edu.uniandes.estamparte.estampa.logic.dto.EstampaDTO;
+import java.util.List;
+
 
 public interface IEstampaLogic {
     
+    //Crea una nueva estampa
+    public EstampaDTO crearEstampa(EstampaDTO estampa);
+    
+    //Elimina una estampa con el id dado
+    public EstampaDTO eliminarEstampa(Long id);
+    
+    //Obtiene las estampas guardadas
+    public List<EstampaDTO> obtenerEstampas();
+    
+    //Obtiene las estampas guardadas de acuerdo a paginacion
+    public List<EstampaDTO> obtenerEstampas(Integer pagina, Integer datosMaximos);
+    
+    //Obtiene las estampas de un artista con un id dado
+    public List<EstampaDTO> obtenerEstampasDeArtista(Long id);
+    
+    //Actualiza la informacion de una estampa
+    public EstampaDTO actualizarEstampa(EstampaDTO estampa);
+    
+   
 }
