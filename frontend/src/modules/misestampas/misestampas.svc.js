9c6d7d9c0e475755aed65c2cdb4b67c7092e785b
Juan Camilo Bages <jcbages@outlook.com>
2015-03-20 11:09:07 -0500

    Modulo misestampas ya es funcional

--- frontend/src/modules/misestampas/misestampas.svc.js
+++ frontend/src/modules/misestampas/misestampas.svc.js
@@ -1,11 +1,50 @@
 (function () {
 	var app = angular.module('misestampasModule');
 
-	app.service('servicioMisestampas', ['CRUDUtils', 'misestampas.context', function (CRUDUtils, context) {
+	app.service('servicioMisestampas', function () {
 
-		this.url = context;
+		var _this = this;
 
-		CRUDUtils.extendService(this);
+		_this.misestampas = [];
 
-	}]);
+		_this.darMisestampas = function () {
+			return _this.misestampas;
+		}
+
+		_this.posicion = function (estampa) {
+			var respuesta = -1;
+			var encontrado = false;
+
+			for (var i = 0; i < _this.misestampas.length && !encontrado; i++) {
+				var actual = _this.misestampas[i];
+				if (actual === estampa) {
+					respuesta = i;
+					encontrado = true;
+				}
+			}
+
+			return respuesta;
+		}
+
+		_this.existe = function (estampa) {
+			return _this.posicion(estampa) !== -1;
+		}
+
+		_this.agregar = function (estampa) {
+			var existe = _this.existe(estampa);
+
+			if (!existe) {
+				_this.misestampas.push(estampa);
+			}
+		}
+
+		_this.eliminar = function (estampa) {
+			var posicion = _this.posicion(estampa);
+
+			if (posicion != -1) {
+				_this.misestampas.splice(posicion, 1);
+			}
+		}
+
+	});
 })();	
