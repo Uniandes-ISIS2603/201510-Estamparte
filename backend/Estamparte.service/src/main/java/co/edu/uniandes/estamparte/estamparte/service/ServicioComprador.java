55cb979b6a0fb87b29407240b44fa0731a0e7f6e
Castrom95 <n.castro15@uniandes.edu.co>
2015-03-26 09:15:21 -0500

    service comprador

--- backend/Estamparte.service/src/main/java/co/edu/uniandes/estamparte/estamparte/service/ServicioComprador.java
+++ backend/Estamparte.service/src/main/java/co/edu/uniandes/estamparte/estamparte/service/ServicioComprador.java
@@ -5,10 +5,57 @@
  */
 package co.edu.uniandes.estamparte.estamparte.service;
 
+import co.edu.uniandes.estamparte.comprador.logic.api.ICompradorLogic;
+import co.edu.uniandes.estamparte.comprador.logic.dto.CompradorDTO;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
  * @author estudiante
  */
+@Path("/comprador")
+@Stateless
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ServicioComprador {
+    @Inject
+    protected ICompradorLogic compradorlogic;
+    
+    @POST
+    public CompradorDTO createComprador(CompradorDTO detalles)
+    {
+        return compradorlogic.createComprador(detalles);
+    }
+     
+    @GET
+    public List<CompradorDTO> getCompradores(){
+        return compradorlogic.getCompradores();
+    }
+    
+    @GET
+    @Path("/{id}")
+    public CompradorDTO getComprador(Long id){
+        return compradorlogic.getComprador(id);
+    }
+    
+    @DELETE
+    public CompradorDTO deleteComprador(Long id){
+        return compradorlogic.deleteComprador(id);
+    }
+    
+    @PUT
+    public void updateComprador(CompradorDTO detalles){
+        compradorlogic.updateComprador(detalles);
+    }
     
 }
