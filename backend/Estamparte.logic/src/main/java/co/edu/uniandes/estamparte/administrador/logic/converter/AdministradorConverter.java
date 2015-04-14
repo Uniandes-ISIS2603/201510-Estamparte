d14b083123bdd9166c6904a42056e9c090204515
Castrom95 <n.castro15@uniandes.edu.co>
2015-04-14 12:10:54 -0500

    converter administrador

--- backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/administrador/logic/converter/AdministradorConverter.java
+++ backend/Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/administrador/logic/converter/AdministradorConverter.java
@@ -5,6 +5,8 @@ package co.edu.uniandes.estamparte.administrador.logic.converter;
 
 //Convierte los objetos de clases de java a DTO (objeto que transfiere archivos a persistencia)
 
+import co.edu.uniandes.estamparte.administrador.logic.dto.AdministradorDTO;
+import co.edu.uniandes.estamparte.administrador.logic.entity.AdministradorEntity;
 import co.edu.uniandes.estamparte.artista.logic.converter.*;
 import co.edu.uniandes.estamparte.artista.logic.dto.ArtistaDTO;
 import co.edu.uniandes.estamparte.artista.logic.entity.ArtistaEntity;
@@ -14,54 +16,48 @@ import java.util.List;
 
 public class AdministradorConverter {
     
-    public static ArtistaDTO convertirDeEntidadADTO (ArtistaEntity entidad) {
+    public static AdministradorDTO convertirDeEntidadADTO (AdministradorEntity entidad) {
         if (entidad != null) {
-            ArtistaDTO dto = new ArtistaDTO();
+            AdministradorDTO dto = new AdministradorDTO();
             dto.setId(entidad.getId());
             dto.setUsuario(entidad.getUsuario());
-            dto.setContrasenha(entidad.getContrasenha());
+            dto.setContrasena(entidad.getContrasena());
             dto.setNombre(entidad.getNombre());
             dto.setCedula(entidad.getCedula());
             dto.setCorreo(entidad.getCorreo());
-            dto.setImagenPerfil(entidad.getImagenPerfil());
-            dto.setDescripcion(entidad.getDescripcion());
             return dto;
         } else {
             return null;
         }
     }
 
-    public static ArtistaEntity convertirDeDTOAEntidad (ArtistaDTO dto) {
+    public static AdministradorEntity convertirDeDTOAEntidad (AdministradorDTO dto) {
         if (dto != null) {
-            ArtistaEntity entidad = new ArtistaEntity();
-            List<EstampaEntity> estampas = new ArrayList();
+            AdministradorEntity entidad = new AdministradorEntity();
             //Al objeto entity se le asignan los atributos del objeto dto
             entidad.setId(dto.getId());
             entidad.setUsuario(dto.getUsuario());
-            entidad.setContrasenha(dto.getContrasenha());
+            entidad.setContrasena(dto.getContrasena());
             entidad.setNombre(dto.getNombre());
             entidad.setCedula(dto.getCedula());
             entidad.setCorreo(dto.getCorreo());
-            entidad.setImagenPerfil(dto.getImagenPerfil());
-            entidad.setDescripcion(dto.getDescripcion());
-            entidad.setEstampas(estampas);
             return entidad;
         } else {
             return null;
         }
     }
 
-    public static List<ArtistaDTO> convertirDeListaEntidadesAListaDTO (List<ArtistaEntity> listaEntities) {
-        List<ArtistaDTO> listaDTO = new ArrayList<ArtistaDTO>();
-        for (ArtistaEntity entity : listaEntities) {
+    public static List<AdministradorDTO> convertirDeListaEntidadesAListaDTO (List<AdministradorEntity> listaEntities) {
+        List<AdministradorDTO> listaDTO = new ArrayList<AdministradorDTO>();
+        for (AdministradorEntity entity : listaEntities) {
             listaDTO.add(convertirDeEntidadADTO(entity));
         }
         return listaDTO;
     }
 
-    public static List<ArtistaEntity> convertirDeListaDTOAListaEntidades (ArrayList<ArtistaDTO> listaDTO) {
-        List<ArtistaEntity> listaEntities = new ArrayList<ArtistaEntity>();
-        for (ArtistaDTO dto : listaDTO) {
+    public static List<AdministradorEntity> convertirDeListaDTOAListaEntidades (ArrayList<AdministradorDTO> listaDTO) {
+        List<AdministradorEntity> listaEntities = new ArrayList<AdministradorEntity>();
+        for (AdministradorDTO dto : listaDTO) {
             listaEntities.add(convertirDeDTOAEntidad(dto));
         }
         return listaEntities;
