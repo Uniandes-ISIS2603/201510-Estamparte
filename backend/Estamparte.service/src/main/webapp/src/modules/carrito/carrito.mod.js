093c1267aca0662e9e22803d80352318856fe68a
Meili Vanegas <m.vanegas10@uniandes.edu.co>
2015-04-20 17:42:47 -0500

    Definición de constantes para integrar Carrito y FP

--- backend/Estamparte.service/src/main/webapp/src/modules/carrito/carrito.mod.js
+++ backend/Estamparte.service/src/main/webapp/src/modules/carrito/carrito.mod.js
@@ -3,7 +3,14 @@
 
 	app.constant('carrito.context', 'carrito');
 
+        app.constant('carrito.skipMock', true);
+
+        app.config(['carrito.context', 'MockModule.urlsProvider','carrito.skipMock', function (context, urlsProvider, skipMock) {
+                urlsProvider.registerUrl(context, skipMock);
+        }]);
+
 	app.config(['carrito.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
 		urlsProvider.registerUrl(context);
 	}]);
 })();
+
