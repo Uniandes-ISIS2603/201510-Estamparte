7280ee349ea4ab7628ba5fd2dd4b6448286b4c07
Castrom95 <n.castro15@uniandes.edu.co>
2015-05-06 11:21:28 -0500

    sd

--- Estamparte.logic/src/test/java/co/edu/uniandes/estamparte/artista/persistence/ArtistaPersistenceTest.java
+++ Estamparte.logic/src/test/java/co/edu/uniandes/estamparte/artista/persistence/ArtistaPersistenceTest.java
@@ -1,20 +1,41 @@
 package co.edu.uniandes.estamparte.artista.persistence;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 import co.edu.uniandes.estamparte.artista.logic.api.IArtistaLogic;
 import co.edu.uniandes.estamparte.artista.logic.converter.ArtistaConverter;
 import co.edu.uniandes.estamparte.artista.logic.dto.ArtistaDTO;
 import co.edu.uniandes.estamparte.artista.logic.dto.ArtistaPageDTO;
 import co.edu.uniandes.estamparte.artista.logic.ejb.ArtistaLogic;
 import co.edu.uniandes.estamparte.artista.logic.entity.ArtistaEntity;
+import co.edu.uniandes.estamparte.camiseta.logic.api.ICamisetaLogic;
+import co.edu.uniandes.estamparte.camiseta.logic.converter.CamisetaConverter;
+import co.edu.uniandes.estamparte.camiseta.logic.dto.CamisetaDTO;
+import co.edu.uniandes.estamparte.camiseta.logic.ejb.CamisetaLogic;
+import co.edu.uniandes.estamparte.camiseta.logic.entity.CamisetaEntity;
+import co.edu.uniandes.estamparte.carrito.logic.api.ICarritoLogic;
+import co.edu.uniandes.estamparte.carrito.logic.converter.CarritoConverter;
+import co.edu.uniandes.estamparte.carrito.logic.dto.CarritoDTO;
+import co.edu.uniandes.estamparte.carrito.logic.ejb.CarritoLogic;
+import co.edu.uniandes.estamparte.carrito.logic.entity.CarritoEntity;
+import co.edu.uniandes.estamparte.comprador.logic.api.ICompradorLogic;
+import co.edu.uniandes.estamparte.comprador.logic.converter.CompradorConverter;
+import co.edu.uniandes.estamparte.comprador.logic.dto.CompradorDTO;
+import co.edu.uniandes.estamparte.comprador.logic.ejb.CompradorLogic;
+import co.edu.uniandes.estamparte.comprador.logic.entity.CompradorEntity;
 import co.edu.uniandes.estamparte.estampa.logic.api.IEstampaLogic;
 import co.edu.uniandes.estamparte.estampa.logic.converter.EstampaConverter;
 import co.edu.uniandes.estamparte.estampa.logic.dto.EstampaDTO;
 import co.edu.uniandes.estamparte.estampa.logic.ejb.EstampaLogic;
 import co.edu.uniandes.estamparte.estampa.logic.entity.EstampaEntity;
+import co.edu.uniandes.estamparte.factura.logic.api.IFacturaLogic;
+import co.edu.uniandes.estamparte.factura.logic.converter.FacturaConverter;
+import co.edu.uniandes.estamparte.factura.logic.dto.FacturaDTO;
+import co.edu.uniandes.estamparte.factura.logic.ejb.FacturaLogic;
+import co.edu.uniandes.estamparte.factura.logic.entity.FacturaEntity;
+import co.edu.uniandes.estamparte.formaPago.logic.api.IFormaPagoLogic;
+import co.edu.uniandes.estamparte.formaPago.logic.converter.FormaPagoConverter;
+import co.edu.uniandes.estamparte.formaPago.logic.dto.FormaPagoDTO;
+import co.edu.uniandes.estamparte.formaPago.logic.ejb.FormaPagoLogic;
+import co.edu.uniandes.estamparte.formaPago.logic.entity.FormaPagoEntity;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -44,12 +65,48 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  */
 @RunWith(Arquillian.class)
 public class ArtistaPersistenceTest {
-    /**
+    
     public static final String DEPLOY = "Prueba";
     @Deployment
     public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class, DEPLOY + ".war")
                 //Aï¿½ade el paquete en el que se encuentra la clase 'SportPersistance.java'
+                .addPackage(ICompradorLogic.class.getPackage())
+                .addPackage(CompradorLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(CompradorEntity.class.getPackage())
+                .addPackage(CompradorDTO.class.getPackage())
+                .addPackage(CompradorConverter.class.getPackage())
+                .addPackage(IFacturaLogic.class.getPackage())
+                .addPackage(FacturaLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(FacturaEntity.class.getPackage())
+                .addPackage(FacturaDTO.class.getPackage())
+                .addPackage(FacturaConverter.class.getPackage())
+                .addPackage(IFormaPagoLogic.class.getPackage())
+                .addPackage(FormaPagoLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(FormaPagoEntity.class.getPackage())
+                .addPackage(FormaPagoDTO.class.getPackage())
+                .addPackage(FormaPagoConverter.class.getPackage())
+                .addPackage(IEstampaLogic.class.getPackage())
+                .addPackage(EstampaLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(EstampaEntity.class.getPackage())
+                .addPackage(EstampaDTO.class.getPackage())
+                .addPackage(EstampaConverter.class.getPackage())
+                .addPackage(ICarritoLogic.class.getPackage())
+                .addPackage(CarritoLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(CarritoEntity.class.getPackage())
+                .addPackage(CarritoDTO.class.getPackage())
+                .addPackage(CarritoConverter.class.getPackage())
+                .addPackage(ICamisetaLogic.class.getPackage())
+                .addPackage(CamisetaLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(CamisetaEntity.class.getPackage())
+                .addPackage(CamisetaDTO.class.getPackage())
+                .addPackage(CamisetaConverter.class.getPackage())
                 .addPackage(IArtistaLogic.class.getPackage())
                 .addPackage(ArtistaLogic.class.getPackage())
                 //Aï¿½ade el paquete en el que se encuentra la clase 'SportEntity.java'
@@ -58,17 +115,8 @@ public class ArtistaPersistenceTest {
                                 .addPackage(ArtistaPageDTO.class.getPackage())
 
                 .addPackage(ArtistaConverter.class.getPackage())
-<<<<<<< HEAD
-                 .addPackage(IEstampaLogic.class.getPackage())
-                .addPackage(EstampaLogic.class.getPackage())
-                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
-                .addPackage(EstampaEntity.class.getPackage())
-                .addPackage(EstampaDTO.class.getPackage())
-                .addPackage(EstampaConverter.class.getPackage())
                 //Finalmente se añaden los archivos persistance.xml y beans.xml para la Unidad de peristencia y CDI del paquete mínimo
-=======
                 //Finalmente se aï¿½aden los archivos persistance.xml y beans.xml para la Unidad de peristencia y CDI del paquete mï¿½nimo
->>>>>>> origin/master
                 .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
                 .addAsWebInfResource(new File ("src/main/resources/META-INF/beans.xml"));
     }
@@ -120,6 +168,6 @@ public class ArtistaPersistenceTest {
         
         junit.framework.Assert.assertTrue(true);
     }
-   */
+   
     
 }
