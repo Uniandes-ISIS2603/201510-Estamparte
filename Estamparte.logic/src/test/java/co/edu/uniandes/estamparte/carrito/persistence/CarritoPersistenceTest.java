8927048b17d3ac5610b84f50c1057e2a8adf16c8
Castrom95 <n.castro15@uniandes.edu.co>
2015-05-07 12:16:41 -0500

    intento arreglar test carrito

--- Estamparte.logic/src/test/java/co/edu/uniandes/estamparte/carrito/persistence/CarritoPersistenceTest.java
+++ Estamparte.logic/src/test/java/co/edu/uniandes/estamparte/carrito/persistence/CarritoPersistenceTest.java
@@ -6,6 +6,12 @@ import co.edu.uniandes.estamparte.administrador.logic.dto.AdministradorDTO;
 import co.edu.uniandes.estamparte.administrador.logic.ejb.AdministradorLogic;
 import co.edu.uniandes.estamparte.administrador.logic.entity.AdministradorEntity;
 import co.edu.uniandes.estamparte.administrador.persistence.*;
+import co.edu.uniandes.estamparte.artista.logic.api.IArtistaLogic;
+import co.edu.uniandes.estamparte.artista.logic.converter.ArtistaConverter;
+import co.edu.uniandes.estamparte.artista.logic.dto.ArtistaDTO;
+import co.edu.uniandes.estamparte.artista.logic.dto.ArtistaPageDTO;
+import co.edu.uniandes.estamparte.artista.logic.ejb.ArtistaLogic;
+import co.edu.uniandes.estamparte.artista.logic.entity.ArtistaEntity;
 import co.edu.uniandes.estamparte.camiseta.logic.api.ICamisetaLogic;
 import co.edu.uniandes.estamparte.camiseta.logic.converter.CamisetaConverter;
 import co.edu.uniandes.estamparte.camiseta.logic.dto.CamisetaDTO;
@@ -16,12 +22,29 @@ import co.edu.uniandes.estamparte.carrito.logic.converter.CarritoConverter;
 import co.edu.uniandes.estamparte.carrito.logic.dto.CarritoDTO;
 import co.edu.uniandes.estamparte.carrito.logic.ejb.CarritoLogic;
 import co.edu.uniandes.estamparte.carrito.logic.entity.CarritoEntity;
+import co.edu.uniandes.estamparte.comprador.logic.api.ICompradorLogic;
+import co.edu.uniandes.estamparte.comprador.logic.converter.CompradorConverter;
+import co.edu.uniandes.estamparte.comprador.logic.dto.CompradorDTO;
+import co.edu.uniandes.estamparte.comprador.logic.ejb.CompradorLogic;
+import co.edu.uniandes.estamparte.comprador.logic.entity.CompradorEntity;
 import co.edu.uniandes.estamparte.estampa.logic.api.IEstampaLogic;
 import co.edu.uniandes.estamparte.estampa.logic.converter.EstampaConverter;
 import co.edu.uniandes.estamparte.estampa.logic.dto.EstampaDTO;
 import co.edu.uniandes.estamparte.estampa.logic.ejb.EstampaLogic;
 import co.edu.uniandes.estamparte.estampa.logic.entity.EstampaEntity;
+import co.edu.uniandes.estamparte.factura.logic.api.IFacturaLogic;
+import co.edu.uniandes.estamparte.factura.logic.converter.FacturaConverter;
+import co.edu.uniandes.estamparte.factura.logic.dto.FacturaDTO;
+import co.edu.uniandes.estamparte.factura.logic.ejb.FacturaLogic;
+import co.edu.uniandes.estamparte.factura.logic.entity.FacturaEntity;
+import co.edu.uniandes.estamparte.formaPago.logic.api.IFormaPagoLogic;
+import co.edu.uniandes.estamparte.formaPago.logic.converter.FormaPagoConverter;
+import co.edu.uniandes.estamparte.formaPago.logic.dto.FormaPagoDTO;
+import co.edu.uniandes.estamparte.formaPago.logic.ejb.FormaPagoLogic;
+import co.edu.uniandes.estamparte.formaPago.logic.entity.FormaPagoEntity;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -30,6 +53,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -47,12 +71,36 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class CarritoPersistenceTest {
-    /**
+    
      public static final String DEPLOY = "Prueba";
     @Deployment
     public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class, DEPLOY + ".war")
                 //Añade el paquete en el que se encuentra la clase 'SportPersistance.java'
+                .addPackage(ICompradorLogic.class.getPackage())
+                .addPackage(CompradorLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(CompradorEntity.class.getPackage())
+                .addPackage(CompradorDTO.class.getPackage())
+                .addPackage(CompradorConverter.class.getPackage())
+                .addPackage(IFacturaLogic.class.getPackage())
+                .addPackage(FacturaLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(FacturaEntity.class.getPackage())
+                .addPackage(FacturaDTO.class.getPackage())
+                .addPackage(FacturaConverter.class.getPackage())
+                .addPackage(IFormaPagoLogic.class.getPackage())
+                .addPackage(FormaPagoLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(FormaPagoEntity.class.getPackage())
+                .addPackage(FormaPagoDTO.class.getPackage())
+                .addPackage(FormaPagoConverter.class.getPackage())
+                .addPackage(IEstampaLogic.class.getPackage())
+                .addPackage(EstampaLogic.class.getPackage())
+                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(EstampaEntity.class.getPackage())
+                .addPackage(EstampaDTO.class.getPackage())
+                .addPackage(EstampaConverter.class.getPackage())
                 .addPackage(ICarritoLogic.class.getPackage())
                 .addPackage(CarritoLogic.class.getPackage())
                 //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
@@ -65,24 +113,57 @@ public class CarritoPersistenceTest {
                 .addPackage(CamisetaEntity.class.getPackage())
                 .addPackage(CamisetaDTO.class.getPackage())
                 .addPackage(CamisetaConverter.class.getPackage())
-                .addPackage(IEstampaLogic.class.getPackage())
-                .addPackage(EstampaLogic.class.getPackage())
-                //Añade el paquete en el que se encuentra la clase 'SportEntity.java'
-                .addPackage(EstampaEntity.class.getPackage())
-                .addPackage(EstampaDTO.class.getPackage())
-                .addPackage(EstampaConverter.class.getPackage())
+                .addPackage(IArtistaLogic.class.getPackage())
+                .addPackage(ArtistaLogic.class.getPackage())
+                //Aï¿½ade el paquete en el que se encuentra la clase 'SportEntity.java'
+                .addPackage(ArtistaEntity.class.getPackage())
+                .addPackage(ArtistaDTO.class.getPackage())
+                .addPackage(ArtistaPageDTO.class.getPackage())
+
+                .addPackage(ArtistaConverter.class.getPackage())
                 //Finalmente se añaden los archivos persistance.xml y beans.xml para la Unidad de peristencia y CDI del paquete mínimo
                 .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
                 .addAsWebInfResource(new File ("src/main/resources/META-INF/beans.xml"));
     }
     
     @Inject
-     private ICarritoLogic adminPersistence;
+     private ICarritoLogic carritoPersistence;
      @PersistenceContext
      private EntityManager em;
      @Inject
      UserTransaction utx;
     
+      @Before
+    public void configTest() {
+        System.out.println("em: " + em);
+        try {
+            utx.begin();
+            clearData();
+            //insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private void clearData() {
+        em.createQuery("delete from CarritoEntity").executeUpdate();
+    }
+
+    private List<CarritoEntity> data = new ArrayList<CarritoEntity>();
+
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            CarritoEntity entity = new CarritoEntity();
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
     
     
     
@@ -92,7 +173,9 @@ public class CarritoPersistenceTest {
     @Test
     public void createCarritoTest() {
         // se instancia el generador de datos Podam
-        
+        CarritoDTO dtoo = new CarritoDTO();
+        CarritoDTO res = carritoPersistence.crearCarrito(dtoo);
+   
     }
     
     @Test
@@ -108,5 +191,5 @@ public class CarritoPersistenceTest {
     @Test
     public void updateCarritoTest(){
         
-    } */
+    } 
 }
