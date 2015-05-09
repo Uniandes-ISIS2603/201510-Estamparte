5f12f629731f76b5da71f805054f3c467cc181d1
jsmarti <jsmarti@hotmail.com>
2015-05-09 12:47:19 -0500

    Adicion de persistencia de estampas
    Adicion de clases necesarias para guardar imagenes de estampas en el servidor

---
+++ Estamparte.logic/src/main/java/co/edu/uniandes/estamparte/estampaImgPersistence/logic/ejb/ImgPersistenceLogic.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.estamparte.estampaImgPersistence.logic.ejb;
+
+import co.edu.uniandes.estamparte.estampaImgPersistence.logic.api.IImgPersistenceLogic;
+import co.edu.uniandes.estamparte.estampaImgPersistence.logic.dto.ImgDTO;
+
+/**
+ *
+ * @author JsMarti
+ */
+public class ImgPersistenceLogic implements IImgPersistenceLogic {
+
+    @Override
+    public String createEstampaImagen(ImgDTO img) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String updateImagen(String url) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteImagen(String url) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
